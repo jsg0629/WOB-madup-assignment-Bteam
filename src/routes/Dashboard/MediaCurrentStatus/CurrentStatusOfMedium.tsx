171ce34d9a12a92f22421e86723068aadab2d496
jsg0629 <tnrms513@gmail.com>
2022-05-23 19:26:23 +0900

    docs: CurrentStatusOfMedium component추가

---
+++ src/routes/Dashboard/MediaCurrentStatus/CurrentStatusOfMedium.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from './CurrentStatusOfMedium.module.scss'
+
+const CurrentStatusOfMedium = () => {
+  return <div>CurrentStatusOfMedium</div>
+}
+
+export default CurrentStatusOfMedium
--- src/routes/Dashboard/index.tsx
+++ src/routes/Dashboard/index.tsx
@@ -1,7 +1,12 @@
 import styles from './dashboard.module.scss'
+import CurrentStatusOfMedium from './MediaCurrentStatus/CurrentStatusOfMedium'
 
 const Dashboard = (): JSX.Element => {
-  return <div>Dashboard</div>
+  return (
+    <div>
+      <div>Dashboard</div>
+      <CurrentStatusOfMedium />
+    </div>
+  )
 }
-
 export default Dashboard
