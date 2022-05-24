fa6a2fdc3bc42d7db2fc7f1ce046a114278b4ef4
Seohyun Roh <radult951@gmail.com>
2022-05-25 01:55:52 +0900

    feat: 광고현황 일간 차트

---
+++ src/routes/Dashboard/Chart/ChartItem.tsx
@@ -0,0 +1,128 @@
+import { VictoryAxis, VictoryChart, VictoryLabel, VictoryLine, VictoryTheme } from 'victory'
+
+import styles from './chart.module.scss'
+
+type Data = {
+  x: string
+  y: number
+}
+
+interface Props {
+  firstData?: Data[]
+  secondData?: Data[]
+}
+
+const ChartItem = ({ firstData, secondData }: Props): JSX.Element => {
+  const options = {
+    width: 960,
+    height: 400,
+  }
+
+  const maxNum = (d: Data[]) => {
+    return d.reduce((max, p) => (p.y > max ? p.y : max), d[0].y)
+  }
+
+  const maxima = (mNum: number) => {
+    const strMaxNum = Math.floor(mNum).toString()
+    const firstDigit = Number(strMaxNum.substring(0, 1)) + 1
+    const square = 10 ** (strMaxNum.length - 1)
+
+    return firstDigit * square
+  }
+
+  const data = []
+
+  if (firstData) {
+    data[0] = firstData
+  } else {
+    data[0] = null
+  }
+
+  if (secondData) {
+    data[1] = secondData
+  } else {
+    data[1] = null
+  }
+
+  const xOffsets = [50, 910]
+  const colors = ['#4fadf7', '#85da47']
+
+  return (
+    <div className={styles.chartContainer}>
+      {firstData && firstData.length !== 0 && (
+        <VictoryChart theme={VictoryTheme.material} {...options} domain={{ y: [0, 1] }}>
+          <VictoryAxis />
+          {data?.map((d, i) => {
+            if (d) {
+              const key = `victoryAxis-${i}`
+              return (
+                <VictoryAxis
+                  key={key}
+                  dependentAxis
+                  offsetX={xOffsets[i]}
+                  tickLabelComponent={<VictoryLabel dy={15} textAnchor='start' />}
+                  style={{
+                    axis: { stroke: 'none' },
+                    tickLabels: { fill: 'black' },
+                  }}
+                  tickValues={[0.2, 0.4, 0.6, 0.8, 1]}
+                  tickFormat={(t) => (t * maxima(maxNum(d))).toLocaleString()}
+                />
+              )
+            }
+            return null
+          })}
+          {data?.map((d, i) => {
+            if (d) {
+              const key = `victoryLine-${i}`
+              return (
+                <VictoryLine
+                  key={key}
+                  data={d}
+                  animate={{
+                    duration: 2000,
+                    onLoad: { duration: 1000 },
+                  }}
+                  style={{
+                    parent: { border: '1px solid #ccc' },
+                    data: { stroke: colors[i] },
+                  }}
+                  y={(datum) => datum.y / maxima(maxNum(d))}
+                />
+              )
+            }
+            return null
+          })}
+        </VictoryChart>
+      )}
+    </div>
+  )
+}
+
+export default ChartItem
+{
+  /* <VictoryAxis
+              dependentAxis
+              offsetX={910}
+              tickLabelComponent={<VictoryLabel dy={15} textAnchor='start' />}
+              style={{
+                axis: { stroke: 'none' },
+                tickLabels: { fill: 'black' },
+              }}
+              tickValues={[0.2, 0.4, 0.6, 0.8, 1]}
+              tickFormat={(t) => (t * maxima(maxNum(secondData))).toLocaleString()}
+            />
+            <VictoryLine
+              name={`${secondData}`}
+              data={secondData}
+              animate={{
+                duration: 2000,
+                onLoad: { duration: 1000 },
+              }}
+              style={{
+                data: { stroke: 'red' },
+                parent: { border: '1px solid #ccc' },
+              }}
+              y={(datum) => datum.y / maxima(maxNum(secondData))}
+            /> */
+}
---
+++ src/routes/Dashboard/Chart/index.tsx
@@ -0,0 +1,58 @@
+import { useState } from 'react'
+
+import { useRecoilState } from 'hooks/state'
+import { dailyDataResultState } from 'states/dashboard'
+import { convertData } from './utils'
+
+import SelectBox from 'routes/AdvertiseManage/SelectBox'
+import ChartItem from './ChartItem'
+import styles from './chart.module.scss'
+
+const SELECT_LIST = ['ROAS', '광고비', '노출 수', '클릭 수', '전환 수', '매출', '없음']
+const PERIOD_SELECT_LIST = ['일간', '주간']
+
+const Chart = (): JSX.Element => {
+  const [dailyData] = useRecoilState(dailyDataResultState)
+
+  const [firstSelect, setFirstSelect] = useState(SELECT_LIST[0])
+  const [secondSelect, setSecondSelect] = useState(SELECT_LIST[0])
+  const [periodSelect, setPeriodSelect] = useState(PERIOD_SELECT_LIST[0])
+
+  const { roas, cost, imp, click, conv } = convertData(dailyData)
+
+  const firstData = {
+    ROAS: roas,
+    광고비: cost,
+    '노출 수': imp,
+    '클릭 수': click,
+    '전환 수': conv,
+  }[firstSelect]
+
+  const secondData = {
+    ROAS: roas,
+    광고비: cost,
+    '노출 수': imp,
+    '클릭 수': click,
+    '전환 수': conv,
+  }[secondSelect]
+
+  return (
+    <>
+      <div className={styles.selectBoxesContainer}>
+        <div className={styles.selectBoxWrapper}>
+          <SelectBox selectList={SELECT_LIST} currentSelect={firstSelect} setCurrentSelect={setFirstSelect} />
+          <SelectBox selectList={SELECT_LIST} currentSelect={secondSelect} setCurrentSelect={setSecondSelect} />
+        </div>
+        <div>
+          <SelectBox selectList={PERIOD_SELECT_LIST} currentSelect={periodSelect} setCurrentSelect={setPeriodSelect} />
+        </div>
+      </div>
+
+      <div className={styles.chartContainer}>
+        <ChartItem firstData={firstData} secondData={secondData} />
+      </div>
+    </>
+  )
+}
+
+export default Chart
