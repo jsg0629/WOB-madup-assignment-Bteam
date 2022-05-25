9e67f2a4dc74909d0ee2eeda8ce21697674fece5
hyebin829 <smilerain779@gmail.com>
2022-05-25 15:44:10 +0900

    Feat: 선택한 기간 이전 데이터와 비교

--- src/routes/Dashboard/AdTop.tsx
+++ src/routes/Dashboard/AdTop.tsx
@@ -1,60 +1,47 @@
 import { DecreaseIcon, IncreaseIcon } from 'assets/svgs'
+import { useRecoilState } from 'recoil'
+import { dailyDataResultState, prevDailyDataResultState } from 'states/dashboard'
+import { ICardContentData } from 'types/dashboard'
+import { dataProcess, processCardData } from 'utils/adDataProcess'
 import styles from './dashboard.module.scss'
+import store from 'store'
+import { useEffect } from 'react'
 
 const AdTop = () => {
-  const dummyData = [
-    {
-      title: 'ROAS',
-      value: '00000',
-      change: '00000',
-      increase: true,
-    },
-    {
-      title: '광고비',
-      value: '00000',
-      change: '00000',
-      increase: false,
-    },
-    {
-      title: '노출 수',
-      value: '00000',
-      change: '00000',
-      increase: true,
-    },
-    {
-      title: '클릭수',
-      value: '00000',
-      change: '00000',
-      increase: true,
-    },
-    {
-      title: '전환 수',
-      value: '00000',
-      change: '00000',
-      increase: false,
-    },
-    {
-      title: '매출',
-      value: '00000',
-      change: '00000',
-      increase: true,
-    },
-  ]
+  const currentStartDate = store.get('startDate')
+  const currentEndDate = store.get('endDate')
+  const prevStartDate = store.get('prevStartDate')
+  const prevEndDate = store.get('prevEndDate')
+  const [dailyData] = useRecoilState(dailyDataResultState)
+  const [prevDailyData] = useRecoilState(prevDailyDataResultState)
+  const sumData = dataProcess(currentStartDate, currentEndDate, dailyData)
+  const sumPrevData = dataProcess(prevStartDate, prevEndDate, prevDailyData)
+
+  const adCardContent: ICardContentData[] = processCardData(sumData, sumPrevData)
+
+  if (!store.get('adCardData')) store.set('adCardData', [])
+
+  useEffect(() => {
+    store.set('adCardData', adCardContent)
+  }, [adCardContent, currentStartDate, currentEndDate, prevEndDate, prevStartDate])
+
+  const adCardData: ICardContentData[] = store.get('adCardData')
 
   return (
     <ul className={styles.indicatorWrapper}>
-      {dummyData.map((item) => (
-        <li key={item.title} className={styles.indicatorCard}>
-          <dl>
-            <dt>{item.title}</dt>
-            <dd>{item.value}</dd>
-          </dl>
-          <div className={styles.changeWrapper}>
-            {item.increase ? <IncreaseIcon /> : <DecreaseIcon />}
-            <span>{item.change}</span>
-          </div>
-        </li>
-      ))}
+      {adCardData.length > 0 &&
+        adCardData.map((item) => (
+          <li key={item.title} className={styles.indicatorCard}>
+            <dl>
+              <dt>{item.title}</dt>
+              <dd>{item.value}</dd>
+            </dl>
+            <div className={styles.changeWrapper}>
+              {item.increase ? <IncreaseIcon /> : <DecreaseIcon />}
+              <span>{item.change}</span>
+            </div>
+          </li>
+        ))}
     </ul>
   )
 }
